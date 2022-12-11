ce3496fc186bf6f5c9bd483ef780ec6d40b259d9
AaronHernandezPortfolio <103552269+AaronHernandezPortfolio@users.noreply.github.com>
2022-12-10 22:20:17 -0800

    last version

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,8 @@
 import Navbar from '../components/Navbar'
 import React from 'react'
 import { motion, useScroll, useTransform } from 'framer-motion'
+import Image from 'next/image'
+
 
 function about() {
 
@@ -13,21 +15,26 @@ return (
             initial={{ y:"100%" }} animate={{ y:"0%" }} 
             exit={{ opacity: 1  }}
             transition={{duration: .75, ease: "easeOut"}}>
-              <div className="text-white">
+              <div className="relative flex-col bg-center bg-contain h-3/4">
+                <img src="/portfolio.png" alt="portfolio" width="75%" height="100%" className="absolute object-cover w-full h-full opacity-50 -z-10"/>
+              <div classname="z-50 ">
+              <div className="z-10 text-5xl text-white top-7">
                   <h1>Hello World, I'm Sto</h1>
               </div>
-              <div className="text-white">
+              <div className="z-10 text-3xl text-white">
                   <h2>Welcome</h2>
                   <h2>To</h2>
               </div>
-              <div className="text-3xl text-white">
+              <div className="z-10 text-6xl font-semibold text-white">
               <h2> Sto's Digital Solutions'</h2>
               </div>
-              <div className="text-white">
-                  <h2>I specielize in making your vision into reality with a zero's, one's and a bit of coffee.</h2>
-                  <h3 className="text-4xl">
+              <div className="z-10 text-white">
+                  <h2>I specialize in making your vision reality with zero's, one's and a bit of coffee.</h2>
+                  <h3 className="z-10 text-4xl font-bold text-white">
                   『☕』
                   </h3>
+              </div>    
+              </div>
               </div>
         </motion.main>
     </>
--- pages/contact.js
+++ pages/contact.js
@@ -15,14 +15,15 @@ function contact() {
     exit={{ opacity: 1 }} style={{scale}}
     transition={{duration: .75, ease: "easeOut"}}
     >   
-        <div className="flex flex-col justify-center w-full h-full text-center bg-black align-center ">
+        <div className="flex flex-col justify-center w-full text-center bg-fixed bg-black bg-center h-3/4 align-center ">
+          <img src="/contact.jpg" alt="portfolio" width="100%" height="100%" className="absolute object-cover w-full h-full opacity-50 -z-10"/>
         <div className="z-50 flex flex-col text-white translate-y-[15rem]">
-            <h1 className="text-6xl">Lets Talk</h1>
-            <ul className="top-19">
-                <Link href="mailto:sto@stosdigital.com" className="text-2xl text-white "> <h2> <b>Email:</b> sto@stosdigital.com</h2></Link>
-                <Link href="https://www.linkedin.com/in/sto-hernandez/" target="_blank" className="text-2xl text-white"> <h2> <b>LinkedIn:</b> www.linkedin.com/in/sto-hernandez</h2></Link>
-                <Link href="https://www.instagram.com/stos_digital_solutions/" target="_blank" className="text-2xl text-white"> <h2> <b>Instagram:</b> stos_digital_solutions </h2></Link>
-                <Link href="https://twitter.com/StosDigital" target="_blank" className="text-2xl text-white"> <h2> <b>Twitter:</b> StosDigital</h2></Link>
+            <h1 className="text-6xl font-bold text-white drop-shadow-lg shadow-black">Lets Talk</h1>
+            <ul className="top-14">
+                <Link href="mailto:sto@stosdigital.com" className="text-3xl text-white "> <h2> <b>Email:</b> sto@stosdigital.com</h2></Link>
+                <Link href="https://www.linkedin.com/in/sto-hernandez/" target="_blank" className="text-3xl text-white"> <h2> <b>LinkedIn:</b> www.linkedin.com/in/sto-hernandez</h2></Link>
+                <Link href="https://www.instagram.com/stos_digital_solutions/" target="_blank" className="text-3xl text-white"> <h2> <b>Instagram:</b> stos_digital_solutions </h2></Link>
+                <Link href="https://twitter.com/StosDigital" target="_blank" className="text-3xl text-white"> <h2> <b>Twitter:</b> StosDigital</h2></Link>
             </ul>
         </div>
         </div>
